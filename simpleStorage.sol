b583bbc34dbb5bbf2bcc462f65c591a2fb9446aa
akshit-pundir <akshit.pundir90@gmail.com>
2025-11-15 18:15:14 +0530

    new functions and deploying

--- simpleStorage.sol
+++ simpleStorage.sol
@@ -4,17 +4,34 @@ pragma solidity 0.8.30;
 
 contract myContract {
 
-    uint256 public myfavNumber;
+    uint256 myfavNumber;
+    // uint256[] listOfFavNumbers;
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    // Person public pat = Person(7,"Pat");
+
+    // dynamic array
+    Person[] public listOfPeople;
+
+    mapping(string => uint256) public nameToFavNumber;    
 
     function store(uint256 _favNumber) public {
         myfavNumber = _favNumber;
     }
     
+    
     function retrieve() public view returns(uint256){
         return myfavNumber;
     } 
 
+    function addPerson(uint256 num,string memory _name) public {
 
+        listOfPeople.push( Person(num,_name) );
+        nameToFavNumber[_name] = num;
+    }
 
 }
 
