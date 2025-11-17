29852bf435b4632b0ff9fc838e09031ba91bf9b4
akshit-pundir <akshit.pundir90@gmail.com>
2025-11-17 18:52:13 +0530

    #

--- contracts/storageFactory.sol
+++ contracts/storageFactory.sol
@@ -1,40 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-import { myContract } from "./simpleStorage.sol";
+import {myContract} from "./simpleStorage.sol";
 
-contract storageFactory  {
-
-    myContract public simpleStorageContract;
+contract storageFactory {
+    myContract[] public simpleStorageContractList;
 
     function createSimpleStorage() public {
-       
-       simpleStorageContract = new myContract(); 
+        myContract newSimpleStorage = new myContract();
+        simpleStorageContractList.push(newSimpleStorage);
+    }
+
+    function sfStore(uint256 _simpleStorageIdx,uint256 _newSimpleStorageNum) public {
+        
+        myContract mySimpleStorage = simpleStorageContractList[_simpleStorageIdx];
+        mySimpleStorage.store(_newSimpleStorageNum);
 
     }
 
-
-
-
-
+    function sfGet(uint256 _simpleStorageIdx) public view returns(uint256) {  
+        myContract mySimpleStorage = simpleStorageContractList[_simpleStorageIdx];
+        return  mySimpleStorage.retrieve();  
+    }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
